13abf3004b7e8a89ec0dede421db0e2aa59023f4
Aaron Routh <aaronrouth23@gmail.com>
2023-08-17 17:12:15 -0700

    create main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ def main():
     # make your code below
     # print('Hello World')
     ##############################
-
+    print ('Hello World')
+    print ('CS7: Python Programming')
 
 if __name__ == '__main__':
     main()
+    
